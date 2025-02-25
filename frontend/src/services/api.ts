26cb4041a18772a77fb8f7f17980f4e61247a1ef
pedrohhope <pvpswapedro123@gmail.com>
2025-02-25 02:50:12 -0300

    fix: docker

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -7,7 +7,7 @@ export interface Response<T> {
 }
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/src/stories/Header.tsx
+++ frontend/src/stories/Header.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { Button } from './Button';
 import './header.css';
 
@@ -47,7 +45,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
         ) : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button size="small" onClick={onCreateAccount} label="Sign up" />
           </>
         )}
       </div>
